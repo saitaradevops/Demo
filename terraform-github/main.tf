61689ed93b4dd629554fd44a104d0a353eac216b
saitara <saitarasanapati@gmail.com>
2024-06-25 01:52:13 +0530

    Add SG

--- terraform-github/main.tf
+++ terraform-github/main.tf
@@ -2,8 +2,26 @@ resource "aws_instance" "test-server" {
 
   ami           = data.aws_ami.amazon-linux.id
   instance_type = var.instance_type
-
+  subnet_id     = var.subnet_id
   tags = {
     Name = var.tag
   }
+}
+
+resource "aws_security_group" "test-sg" {
+  name        = "Allow SSH"
+  description = "Allow SSH to login"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.test-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "ssh"
+  to_port           = 22
 }
