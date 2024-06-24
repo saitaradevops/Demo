f4122a855d5f95976eb3fc5eececf13a87c9cefc
saitara <saitarasanapati@gmail.com>
2024-06-25 01:58:36 +0530

    Add tcp

--- terraform-github/main.tf
+++ terraform-github/main.tf
@@ -22,6 +22,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.test-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
-  ip_protocol       = "ssh"
+  ip_protocol       = "tcp"
   to_port           = 22
 }
