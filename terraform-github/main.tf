bb8db8fe0669eaf7a80c10c044aebe6c036b4489
saitara <saitarasanapati@gmail.com>
2024-06-25 01:40:57 +0530

    Test

--- terraform-github/main.tf
+++ terraform-github/main.tf
@@ -7,21 +7,3 @@ resource "aws_instance" "test-server" {
     Name = var.tag
   }
 }
-
-
-resource "aws_security_group" "test-server-sg" {
-  name        = "test-server-sg"
-  vpc_id      = var.vpc_id
-  subnet_id = var.subnet_id
-  tags = {
-    Name = "http"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "test-server-sg-rule" {
-  security_group_id = aws_security_group.test-server-sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port   = var.input_port
-  ip_protocol = "http"
-  to_port     = var.input_port
-}
