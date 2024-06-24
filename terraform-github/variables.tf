8cdb35a7b3f02ae4c96ee3aaabdad86e15d82b87
saitara <saitarasanapati@gmail.com>
2024-06-25 01:24:59 +0530

    adding env

--- terraform-github/variables.tf
+++ terraform-github/variables.tf
@@ -21,3 +21,13 @@ variable "input_port" {
 variable "tag" {
     type = string
 }
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
