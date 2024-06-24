1859d5888b574150c46a21a53a2819336bce85d4
saitara <saitarasanapati@gmail.com>
2024-06-24 21:48:15 +0530

    adding terraform code

---
+++ terraform-github/data.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "amazon-linux" {
+  
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.4.*-kernel-6.1-x86_64"]
+  }
+  
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
---
+++ terraform-github/variables.tf
@@ -0,0 +1,23 @@
+variable "instance_type"{
+    type = "string"
+}
+
+variable "region" {
+    type = "string"
+}
+
+variable  "subnet_id" {
+    type = "string"
+}
+
+variable  "vpc_id" {
+    type = "string"
+}
+
+variable "input_port" {
+    type = "number
+}
+
+variable "tag" {
+    type = "string"
+}
